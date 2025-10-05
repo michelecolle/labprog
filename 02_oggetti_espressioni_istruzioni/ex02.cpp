8b08876bf08f73598a688e2b38845b41d4c9725c
michelecolle <michele.colle@studenti.unipd.it>
2025-10-05 20:26:57 +0200

    lette slide lezione 2

--- 02_oggetti_espressioni_istruzioni/ex02.cpp
+++ 02_oggetti_espressioni_istruzioni/ex02.cpp
@@ -1,7 +1,17 @@
 #include <iostream>
 
-int main(void)
+int main()
 {
-std::cout << "Hello, World!\n"; // output "Hello, World!"
+int a {20000};
+char c = a; // char non è grande abbastanza
+int b = c;
+std::cout <<"a: " <<a<< "\n";
+std::cout <<"c: " <<c<< "\n";
+if (a != b)
+std::cout <<a<<" "<<c<< "Ops!\n";
+else
+std::cout << "Wow! Char è molto grande!\n";
+int radice(double x);
+double radice(double x);
 return 0;
 }
