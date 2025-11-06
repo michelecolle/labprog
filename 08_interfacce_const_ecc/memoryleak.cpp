d90c34c156f5dff456441e00f0f0561aa628e652
michelecolle <michele.colle@studenti.unipd.it>
2025-11-06 12:21:38 +0100

    aa

--- 08_interfacce_const_ecc/memoryleak.cpp
+++ 08_interfacce_const_ecc/memoryleak.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Test const interface" << std::endl;
+    std::cout << "Test memory leak" << std::endl;
+    int* leak = new int[1000000]; // Allocating memory without deallocation
+    std::cout << "Allocated memory for 1,000,000 integers." << std::endl;
+    // Intentionally not deleting the allocated memory to simulate a memory leak 
     return 0;
 }
